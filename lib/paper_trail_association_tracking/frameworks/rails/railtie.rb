9f3bbee23ba5069d9e8ef571587e5f95c415ef82
chubchenko <artem.chubchenko@gmail.com>
2022-02-18 11:27:22 +0200

    Patch `Railtie`

--- lib/paper_trail_association_tracking/frameworks/rails/railtie.rb
+++ lib/paper_trail_association_tracking/frameworks/rails/railtie.rb
@@ -3,11 +3,14 @@
 module PaperTrailAssociationTracking
   class Railtie < ::Rails::Railtie
 
-    initializer "paper_trail_association_tracking", before: "load_config_initializers" do
+    initializer "paper_trail_association_tracking", after: "paper_trail" do
       ActiveSupport.on_load(:active_record) do
         require "paper_trail_association_tracking/frameworks/active_record"
       end
     end
 
+    config.to_prepare do
+      ::PaperTrail::Version.include(::PaperTrailAssociationTracking::VersionConcern)
+    end
   end
 end
