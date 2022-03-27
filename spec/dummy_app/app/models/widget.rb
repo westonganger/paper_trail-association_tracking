41814f5a0da70ef264320560ddb1c99c249de4c3
Ben Li <ben@kwelasolutions.com>
2022-03-27 17:16:22 +1100

    Fix the issue that reifying has_one association with "dependent: :destroy" could destroy a live record

--- spec/dummy_app/app/models/widget.rb
+++ spec/dummy_app/app/models/widget.rb
@@ -4,6 +4,7 @@ class Widget < ActiveRecord::Base
   EXCLUDED_NAME = "Biglet"
   has_paper_trail
   has_one :wotsit
+  has_one :bizzo, dependent: :destroy
   has_many(:fluxors, -> { order(:name) })
   has_many :whatchamajiggers, as: :owner
   validates :name, exclusion: { in: [EXCLUDED_NAME] }
